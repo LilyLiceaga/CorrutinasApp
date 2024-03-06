d9e6e1c550fc25251686949629f86ab835d8cd10
LilyLiceaga <lily.liceaga@gmail.com>
2024-03-06 14:37:26 -0600

    Add viewModel to MainActivity

--- app/src/main/java/com/example/corrutinasapp/MainActivity.kt
+++ app/src/main/java/com/example/corrutinasapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.corrutinasapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,18 +13,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.corrutinasapp.ui.theme.CorrutinasAppTheme
 import com.example.corrutinasapp.ui.views.CoroutinesApp
+import com.example.corrutinasapp.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel: MainViewModel by viewModels()
             CorrutinasAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CoroutinesApp()
+                    CoroutinesApp(viewModel = viewModel)
                 }
             }
         }
